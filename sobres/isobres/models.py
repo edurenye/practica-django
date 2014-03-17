6043de4b6538c24f81d13c5c40081e5f4edd9f7d
edurenye <edurenye@hotmail.com>
2014-03-17 12:26:02 +0100

    fi de sobres

--- sobres/isobres/models.py
+++ sobres/isobres/models.py
@@ -4,6 +4,8 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Donor(models.Model):
 	name = models.CharField(max_length=40)
+	def __unicode__(self):
+		return self.name
 
 class Sobre(models.Model):
 	date = models.DateTimeField()
@@ -11,3 +13,5 @@ class Sobre(models.Model):
 	concept = models.TextField(max_length=100)
 	donor = models.ForeignKey(Donor)
 	user = models.ForeignKey(User)
+	def __unicode__(self):
+		return self.donor.name+" - "+self.concept
